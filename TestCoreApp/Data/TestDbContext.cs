5758d0ef15ac1c5f99bec19d5a7654e135d0f4f6
Kirill Lifarenko <kirill.lifarenko@redexpress.ru>
2019-11-30 20:02:47 +0300

    NET.Framework -> NET.Core

---
+++ TestCoreApp/Data/TestDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCoreApp.Data
+{
+    public class TestDbContext : DbContext
+    {
+        public TestDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=WIN-INFMG8OPRNS;Database=Test;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDb;Database=Test;Trusted_Connection=True;");
+        }
+
+        public DbSet<Device> Devices { get; set; }
+        public DbSet<Protocol> Protocols { get; set; }
+    }
+}
---
+++ TestCoreApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using GalaSoft.MvvmLight.CommandWpf;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using TestCoreApp.ViewModels;
+
+namespace TestCoreApp.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {        
+        
+    }
+}
