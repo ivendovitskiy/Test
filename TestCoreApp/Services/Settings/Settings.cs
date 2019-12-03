9eecc73a59ccf0d626d2a8301741eab40ee3f52c
Kirill Lifarenko <kirill.lifarenko@redexpress.ru>
2019-12-03 16:26:43 +0300

    ыфвфывфыв

--- TestCoreApp/Services/Settings/Settings.cs
+++ TestCoreApp/Services/Settings/Settings.cs
@@ -5,15 +5,39 @@ using System.Text.Json.Serialization;
 
 namespace TestCoreApp.Services.Settings
 {
-    public class Settings
+    public class Settings : PropertyChangedBase
     {
+        private string devicesPath;
+        private string responsePath;
+        private string protocolPath;
+        private string connectionString;
+
         [JsonPropertyName("DevicesPath")]
-        public string DevicesPath { get; set; } = "";
+        public string DevicesPath
+        {
+            get => devicesPath;
+            set => Notify(ref devicesPath, value);
+        }
 
         [JsonPropertyName("ResponsePath")]
-        public string ResponsePath { get; set; } = "";
+        public string ResponsePath
+        {
+            get => responsePath;
+            set => Notify(ref responsePath, value);
+        }
 
         [JsonPropertyName("ProtocolPath")]
-        public string ProtocolPath { get; set; } = "";
+        public string ProtocolPath
+        {
+            get => protocolPath;
+            set => Notify(ref protocolPath, value);
+        }
+
+        [JsonPropertyName("ConnectionString")]
+        public string ConnectionString
+        {
+            get => connectionString;
+            set => Notify(ref connectionString, value);
+        }
     }
 }
--- TestCoreApp/Services/Settings/SettingsService.cs
+++ TestCoreApp/Services/Settings/SettingsService.cs
@@ -2,56 +2,70 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 
 namespace TestCoreApp.Services.Settings
 {
-    public class SettingsService
+    public class SettingsService : PropertyChangedBase
     {
         public SettingsService()
         {
-            settingsPath = Path.Combine(Environment.CurrentDirectory,  "settings.json");
+            settingsPath = Path.Combine(Environment.CurrentDirectory, "settings.json");
             LoadSettings();
         }
 
         private readonly string settingsPath;
 
-        public Settings LoadSettings()
+        public void LoadSettings()
         {
             if (File.Exists(settingsPath))
             {
-                using(StreamReader sr = File.OpenText(settingsPath))
+                using (StreamReader sr = File.OpenText(settingsPath))
                 {
-                    Settings = JsonSerializer.Deserialize<Settings>(sr.ReadToEnd(), new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+                    Settings = JsonSerializer.Deserialize<Settings>(sr.ReadToEnd(), new JsonSerializerOptions
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                    });
                 }
             }
             else
             {
                 SaveSettings();
             }
-
-            return new Settings();
         }
 
         public void SaveSettings()
         {
-            using(StreamWriter file = File.CreateText(settingsPath))
+            using (StreamWriter file = File.CreateText(settingsPath))
             {
                 string json;
 
                 if (Settings == null)
                 {
-                    json = JsonSerializer.Serialize(new Settings());
+                    json = JsonSerializer.Serialize(new Settings()
+                    {
+                        ConnectionString = string.Empty,
+                        DevicesPath = string.Empty,
+                        ProtocolPath = string.Empty,
+                        ResponsePath = string.Empty
+                    });
                 }
                 else
                 {
                     json = JsonSerializer.Serialize(Settings);
                 }
-                
-                file.Write(json);                
+
+                file.Write(json);
             }
         }
 
-        public Settings Settings { get; private set; }
+        private Settings settings;
+        public Settings Settings
+        {
+            get => settings;
+            set => Notify(ref settings, value);
+        }
     }
 }
